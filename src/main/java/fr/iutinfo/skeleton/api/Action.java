b45c7b6c601a06485561abd10d9041e87f517811
Leo Vingadassamy <leovinga@hotmail.fr>
2016-10-25 10:43:13 +0200

    action

---
+++ src/main/java/fr/iutinfo/skeleton/api/Action.java
@@ -0,0 +1,19 @@
+package fr.iutinfo.skeleton.api;
+
+public class Action {
+	private int lane;
+	private int champ;
+		
+	public int getLane() {
+		return lane;
+	}
+	public void setLane(int lane) {
+		this.lane = lane;
+	}
+	public int getChamp() {
+		return champ;
+	}
+	public void setChamp(int champ) {
+		this.champ = champ;
+	}
+}
